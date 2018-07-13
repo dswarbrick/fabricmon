b67ff0fddb59c360547b15d790a33d024206cff5
Daniel Swarbrick <daniel.swarbrick@gmail.com>
2018-07-13 14:56:14 +0200

    Move getPortCounters to be a method of ibndNode

--- infiniband/netdiscover.go
+++ infiniband/netdiscover.go
@@ -134,6 +134,92 @@ type ibndNode struct {
 	ibnd_node *C.struct_ibnd_node
 }
 
+// getPortCounters retrieves all counters for a specific port.
+// Note: In PortCounters, PortCountersExtended, PortXmitDataSL, and PortRcvDataSL, components that
+// represent Data (e.g. PortXmitData and PortRcvData) indicate octets divided by 4 rather than just
+// octets.
+func (n *ibndNode) getPortCounters(portId *C.ib_portid_t, portNum int, ibmadPort *C.struct_ibmad_port, resetThreshold uint) (map[uint32]interface{}, error) {
+	var buf [1024]byte
+
+	counters := make(map[uint32]interface{})
+
+	// PerfMgt ClassPortInfo is a required attribute. See ClassPortInfo, IBTA spec v1.3, table 126.
+	pmaBuf := C.pma_query_via(unsafe.Pointer(&buf), portId, C.int(portNum), PMA_TIMEOUT, C.CLASS_PORT_INFO, ibmadPort)
+
+	if pmaBuf == nil {
+		return counters, fmt.Errorf("CLASS_PORT_INFO query failed!")
+	}
+
+	// Keep capMask in network byte order for easier bitwise operations with capabilities contants.
+	capMask := htons(uint16(C.mad_get_field(unsafe.Pointer(&buf), 0, C.IB_CPI_CAPMASK_F)))
+
+	// Fetch standard (32 bit (or less)) counters
+	pmaBuf = C.pma_query_via(unsafe.Pointer(&buf), portId, C.int(portNum), PMA_TIMEOUT, C.IB_GSI_PORT_COUNTERS, ibmadPort)
+
+	if pmaBuf != nil {
+		var selMask uint32
+
+		// Iterate over standard counters
+		for field, counter := range StdCounterMap {
+			if (field == C.IB_PC_XMT_WAIT_F) && (capMask&C.IB_PM_PC_XMIT_WAIT_SUP == 0) {
+				continue // Counter not supported
+			}
+
+			counters[field] = uint32(C.mad_get_field(unsafe.Pointer(&buf), 0, field))
+
+			if float64(counters[field].(uint32)) > (float64(counter.Limit) * float64(resetThreshold) / 100) {
+				log.WithFields(log.Fields{
+					"node_desc": nnMap.RemapNodeName(n.guid(), n.nodeDesc()),
+					"node_guid": n.guidString(),
+					"port":      portNum,
+					"counter":   counter.Name,
+					"value":     counters[field],
+				}).Warn("Counter exceeds threshold")
+				selMask |= counter.Select
+			}
+		}
+
+		if selMask > 0 {
+			var pc [1024]byte
+
+			fields := log.Fields{
+				"node_desc":   nnMap.RemapNodeName(n.guid(), n.nodeDesc()),
+				"node_guid":   n.guidString(),
+				"port":        portNum,
+				"select_mask": fmt.Sprintf("%#x", selMask),
+			}
+
+			log.WithFields(fields).Warn("Resetting counters")
+
+			if C.performance_reset_via(unsafe.Pointer(&pc), portId, C.int(portNum), C.uint(selMask), PMA_TIMEOUT, C.IB_GSI_PORT_COUNTERS, ibmadPort) == nil {
+				log.WithFields(fields).Error("performance_reset_via failed")
+			}
+		}
+	}
+
+	if (capMask&C.IB_PM_EXT_WIDTH_SUPPORTED == 0) && (capMask&C.IB_PM_EXT_WIDTH_NOIETF_SUP == 0) {
+		// TODO: Fetch standard data / packet counters if extended counters are not supported
+		// (pre-QDR hardware).
+		log.WithFields(log.Fields{
+			"node_desc": nnMap.RemapNodeName(n.guid(), n.nodeDesc()),
+			"node_guid": n.guidString(),
+			"port":      portNum,
+		}).Warn("Port does not support extended counters")
+		return counters, nil
+	}
+
+	// Fetch extended (64 bit) counters
+	pmaBuf = C.pma_query_via(unsafe.Pointer(&buf), portId, C.int(portNum), PMA_TIMEOUT, C.IB_GSI_PORT_COUNTERS_EXT, ibmadPort)
+
+	if pmaBuf != nil {
+		for field, _ := range ExtCounterMap {
+			counters[field] = uint64(C.mad_get_field64(unsafe.Pointer(&buf), 0, field))
+		}
+	}
+
+	return counters, nil
+}
+
 func (n *ibndNode) guid() uint64 {
 	return uint64(n.ibnd_node.guid)
 }
@@ -170,8 +256,8 @@ func (n *ibndNode) walkPorts(mad_port *C.struct_ibmad_port, resetThreshold uint)
 	log.WithFields(log.Fields{
 		"node_type": n.ibnd_node._type,
 		"node_desc": nnMap.RemapNodeName(n.guid(), n.nodeDesc()),
-		"num_ports": n.ibnd_node.numports,
 		"node_guid": n.guidString(),
+		"num_ports": n.ibnd_node.numports,
 	}).Debug("Walking ports for node")
 
 	ports := make([]Port, n.ibnd_node.numports+1)
@@ -277,7 +363,7 @@ func (n *ibndNode) walkPorts(mad_port *C.struct_ibmad_port, resetThreshold uint)
 					}
 				*/
 
-				if counters, err := getPortCounters(&portid, portNum, mad_port, resetThreshold); err == nil {
+				if counters, err := n.getPortCounters(&portid, portNum, mad_port, resetThreshold); err == nil {
 					myPort.Counters = counters
 				} else {
 					log.WithError(err).WithFields(log.Fields{
