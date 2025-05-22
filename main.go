0f42b42f48d638ddd882d9ca3d4ed4c4dcd41f02
Daniel Swarbrick <daniel.swarbrick@gmail.com>
2025-05-22 18:34:06 +0200

    Use buffered channel for signal handler

--- main.go
+++ main.go
@@ -86,7 +86,7 @@ func main() {
 	shutdownChan := make(chan bool)
 
 	// Setup signal handler to catch SIGINT, SIGTERM.
-	sigChan := make(chan os.Signal)
+	sigChan := make(chan os.Signal, 1)
 	signal.Notify(sigChan, unix.SIGINT, unix.SIGTERM)
 	go func() {
 		s := <-sigChan
